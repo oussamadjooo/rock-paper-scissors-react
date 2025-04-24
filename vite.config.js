27a017620bb36a25411364cd4189073acc62ddaa
oussamadjooo <oussamadjouider8@gmail.com>
2025-04-24 13:23:24 +0100

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,8 @@
-// vite.config.js
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
+// Base URL for GitHub Pages
 export default defineConfig({
-  base: '/rock-paper-scissors-react/', // 👈 match your GitHub repo name exactly!
+  base: '/rock-paper-scissors-react/',  // Match your GitHub repo name exactly
   plugins: [react()],
 })
